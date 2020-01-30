5f0627380c4a843893c9368ea05b163fc0d12b2d
wheals <shm.mark@gmail.com>
2020-01-30 11:47:18 -0500

    Avoid unnecessary copying of item_defs. Bcadren: Fixed significant merge conflicts in player due to previous changes to the ac display code.
    The struct isn't that small, and if it has any props it can get pretty
    big (orc corpses, for example, may contain an entire monster struct).

--- crawl-ref/source/actor.h
+++ crawl-ref/source/actor.h
@@ -122,7 +122,7 @@ public:
                             bool calc_unid = true) const = 0;
     virtual int scan_artefacts(artefact_prop_type which_property,
                                bool calc_unid = true,
-                               vector<item_def> *matches = nullptr) const = 0;
+                               vector<const item_def *> *matches = nullptr) const = 0;
 
     virtual hands_reqd_type hands_reqd(const item_def &item,
                                        bool base = false) const;
@@ -328,7 +328,7 @@ public:
 
     virtual int res_corr(bool mount = false) const = 0;
     bool has_notele_item(bool calc_unid = true,
-                         vector<item_def> *matches = nullptr) const;
+                         vector<const item_def *> *matches = nullptr) const;
     virtual bool stasis() const = 0;
     virtual bool cloud_immune(bool calc_unid = true, bool items = true) const;
     virtual int run(bool calc_unid = true, bool items = true) const;
