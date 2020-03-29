1985b79f3bc2a0c35a007150e9c690adb58d7528
Aidan Holm <aidanholm@gmail.com>
2020-03-29 15:17:54 +0800

    Remove add_stop parameter from feature_description()

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -7922,8 +7922,7 @@ static void _speech_fill_target(string& targ_prep, string& target,
                 if (grd(pbolt.target) != DNGN_FLOOR)
                 {
                     target = feature_description(grd(pbolt.target),
-                                                 NUM_TRAPS, "", DESC_THE,
-                                                 false);
+                                                 NUM_TRAPS, "", DESC_THE);
                 }
                 else
                     target = "thin air";
