4750dd7b2fe6240bd5979fd4580ce269f1dd9031
Kate <601195+semitonal@users.noreply.github.com>
2020-04-11 21:23:32 +0100

    Clear water elemental engulfing when walljumping

--- crawl-ref/source/god-abil.cc
+++ crawl-ref/source/god-abil.cc
@@ -7652,11 +7652,11 @@ bool wu_jian_do_wall_jump(coord_def targ, bool ability)
     if (!ability)
         count_action(CACT_INVOKE, ABIL_WU_JIAN_WALLJUMP);
     wu_jian_wall_jump_effects();
+    you.clear_far_engulf();
 
     if (ability)
     {
         // TODO: code duplication with movement...
-        // TODO: check engulfing
         int wall_jump_modifier = (you.attribute[ATTR_SERPENTS_LASH] != 1) ? 2
                                                                           : 1;
 
