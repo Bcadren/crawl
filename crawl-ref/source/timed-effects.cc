60368f404a5207529f78d29ceec60e3a895e3135
Edgar A. Bering IV <trizor@gmail.com>
2020-04-18 01:06:53 -0400

    Tone down hell effect miscasts
    A level 5, fail 50 miscast does 125 damage on average with the new
    miscast formulas, and fail was being called with random2avg(97,3) for
    hell effects.
    
    This reduces hell effects to level 5, random2avg(40, 3) miscasts, which
    do a maximum miscast damage of 100, and an average of 50. This will be
    further reduced by a subsequent commit toning down miscasts over all.

--- crawl-ref/source/timed-effects.cc
+++ crawl-ref/source/timed-effects.cc
@@ -82,7 +82,7 @@ static void _random_hell_miscast()
                                  2, spschool::hexes);
 
     miscast_effect(you, nullptr, {miscast_source::hell_effect}, which_miscast,
-                   5, random2avg(97, 3), "the effects of Hell");
+                   5, random2avg(40, 3), "the effects of Hell");
 }
 
 /// The thematically appropriate hell effects for a given hell branch.
@@ -147,7 +147,7 @@ static void _themed_hell_summon_or_miscast()
     else
     {
         miscast_effect(you, nullptr, {miscast_source::hell_effect},
-                      spec->miscast_type, 5, random2avg(97, 3),
+                      spec->miscast_type, 5, random2avg(40, 3),
                       "the effects of Hell");
     }
 }
