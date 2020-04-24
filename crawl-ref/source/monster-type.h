2cb9cac05835f41d833ed855f3239037af4677ec
Aidan Holm <aidanholm@gmail.com>
2020-04-24 18:02:52 +0800

    Re-sync axed / removed monsters

--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -94,7 +94,9 @@ enum monster_type                      // menv[].type
     MONS_BARACHI,
     MONS_BEAR,                  // genus
 #endif
+#if TAG_MAJOR_VERSION == 34
     MONS_GRIZZLY_BEAR,
+#endif
     MONS_POLAR_BEAR,
     MONS_BLACK_BEAR,
     MONS_WORM,
@@ -331,7 +333,9 @@ enum monster_type                      // menv[].type
 #endif
     MONS_BALLISTOMYCETE_SPORE,
     MONS_BALLISTOMYCETE,
+#if TAG_MAJOR_VERSION == 34
     MONS_HYPERACTIVE_BALLISTOMYCETE,
+#endif
 
     MONS_GOBLIN,
     MONS_HOBGOBLIN,
