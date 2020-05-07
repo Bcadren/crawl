e4ee5c35cd5296afce0a032e5f7907606ea904ea
Benoit Giannangeli <benoit.giannangeli@boursorama.fr>
2020-05-07 10:33:06 +0200

    New morgue section `screenshots`
    Notes can have an associated screenshot. For now only user notes take
    screenshots. They are printed out in the `screenshots` section of the
    morgue. A one-screenshot per turn limit is enforced.
    
    [ Committer's note: tweaked for smooth forward upgrade compatibility.
      Squashed an rebased. Closes #1119 ]

--- crawl-ref/source/tag-version.h
+++ crawl-ref/source/tag-version.h
@@ -247,6 +247,7 @@ enum tag_minor_version
     TAG_MINOR_YRED_SPELLS,         // Save spells with enslaved souls. Save monsters spawn items for enslaved soul use.
     TAG_MINOR_MISCAST_MUTATIONS,   // Add unique temporary mutations from miscasts.
     TAG_MINOR_TRACK_REGEN_ITEMS,   // Regen items take effect only after maxhp is reached
+    TAG_MINOR_MORGUE_SCREENSHOTS,  // Screenshots morgue section
 #endif
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
