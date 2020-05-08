5219729c3719217d91dd6497b7221b7767ed7cb0
Edgar A. Bering IV <trizor@gmail.com>
2020-05-08 17:12:33 -0400

    Truncate monster status info cleanly in the console HUD
    Instead of just chopping the string to length, chop a little short and
    write "…)" so the player knows there's more going on and they should
    consult ^x.

--- crawl-ref/source/output.cc
+++ crawl-ref/source/output.cc
@@ -1846,7 +1846,13 @@ static void _print_next_monster_desc(const vector<monster_info>& mons,
             mons_to_string_pane(desc, desc_colour, zombified,
                                 mons, start, count);
             textcolour(desc_colour);
-            desc.resize(crawl_view.mlistsz.x-printed, ' ');
+            if (desc.length() > (unsigned) abs(crawl_view.mlistsz.x - printed))
+            {
+                desc.resize(crawl_view.mlistsz.x - 3 - printed, ' ');
+                desc += "…)";
+            }
+            else
+                desc.resize(crawl_view.mlistsz.x - printed, ' ');
             CPRINTF("%s", desc.c_str());
         }
     }
