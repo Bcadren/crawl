e597dce3ad30bb20df47192c5d1bb3c6374f0da0
advil <rawlins@gmail.com>
2020-05-11 12:58:32 -0400

    Apply location effects *after* temp changing a feature
    ea32a606ab reordered things so that location effects were applied to
    monsters as a side effect before the temporary feature change took
    effect, i.e. using the about-to-be-replaced feature value. Aside from
    not applying any effects of the new feature type to a monster on the
    position, this caused weird things like a water nymph standing on a trap
    and temporarily flooding it to trigger that trap on the temporary
    terrain change. Some example crashes:
    
    http://crawl.berotato.org/crawl/morgue/ardl/crash-ardl-20191231-234514.txt
    http://crawl.akrasiac.org/rawdata/elzilcho/crash-elzilcho-20190927-225210.txt
    
    (The crash is when the terrain change reverts, and the shaft's trap info
    is gone.)
    
    This commit partially reverts ea32a606ab (sorry) but keeps the wall
    check in a more direct way.

--- crawl-ref/source/terrain.cc
+++ crawl-ref/source/terrain.cc
@@ -1194,7 +1194,7 @@ static void _dgn_check_terrain_items(const coord_def &pos, bool preserve_items)
     }
 }
 
-static bool _dgn_check_terrain_monsters(const coord_def &pos)
+static void _dgn_check_terrain_monsters(const coord_def &pos)
 {
     if (monster* m = monster_at(pos))
     {
@@ -1202,10 +1202,7 @@ static bool _dgn_check_terrain_monsters(const coord_def &pos)
             blink_out(m);
         else
             m->apply_location_effects(pos);
-        return true;
     }
-    else
-        return false;
 }
 
 // Clear blood or mold off of terrain that shouldn't have it. Also clear
@@ -1347,6 +1344,7 @@ void dungeon_terrain_changed(const coord_def &pos,
 
     _dgn_check_terrain_monsters(pos);
     _dgn_check_terrain_items(pos, preserve_items);
+    _dgn_check_terrain_monsters(pos);
     if (!wizmode)
         _dgn_check_terrain_player(pos);
     if (!temporary && feature_mimic_at(pos))
