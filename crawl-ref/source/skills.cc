44afc6939a15c7b5645bda04d633080eb603c6b2
advil <rawlins@gmail.com>
2020-05-12 13:02:55 -0400

    Endless warnings

--- crawl-ref/source/skills.cc
+++ crawl-ref/source/skills.cc
@@ -1822,6 +1822,8 @@ bool is_removed_skill(skill_type skill)
 #if TAG_MAJOR_VERSION == 34
     if (skill == SK_STABBING || skill == SK_TRAPS || skill == SK_CONJURATIONS)
         return true;
+#else
+    UNUSED(skill);
 #endif
     return false;
 }
