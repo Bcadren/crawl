3b5db364316f67b1d6c57452690797202c5618cc
advil <rawlins@gmail.com>
2020-05-15 00:11:20 -0400

    Add a placeholder fix for trap+corruption+nymphs
    What seems to be happening is that a trap gets temporarily removed by a
    nymph's flooding, then the terrain change marker gets moved by
    corruption without moving the corresponding env.trap entry, then the
    game crashes when restoring. This fix, which is not intended to be
    final, will instead print an error and place floor on the trap reversion.
    
    I think a real fix should either check and move the trap_def when the
    terrain change marker moves, or remove and recreate env.trap entries in
    sync with any terrain changes. The latter seems better, but I'm not sure
    why it wasn't implemented that way in the beginning, so there may be
    some issue I'm not anticipating.

--- crawl-ref/source/terrain.cc
+++ crawl-ref/source/terrain.cc
@@ -1319,6 +1319,15 @@ void dungeon_terrain_changed(const coord_def &pos,
         mpr("A porticulus slams shut!");
     }
 
+    if (feat_is_trap(nfeat) && env.trap.find(pos) == env.trap.end())
+    {
+        // TODO: create a trap_def in env for this case?
+        mprf(MSGCH_ERROR,
+            "Attempting to change terrain to a trap without a corresponding"
+            " trap_def!");
+        nfeat = DNGN_FLOOR;
+    }
+
     _dgn_check_terrain_covering(pos, grd(pos), nfeat);
 
     if (nfeat != DNGN_UNSEEN)
