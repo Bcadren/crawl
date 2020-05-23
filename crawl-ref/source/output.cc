503fd5e49293b2f05d4d6544768050f9b56a49ab
advil <rawlins@gmail.com>
2020-05-23 15:52:45 -0400

    Fix the tiles build

--- crawl-ref/source/output.cc
+++ crawl-ref/source/output.cc
@@ -1620,6 +1620,7 @@ void draw_border()
 #ifndef USE_TILE_LOCAL
 void redraw_console_sidebar()
 {
+#ifndef USE_TILE_LOCAL
     if (crawl_state.smallterm)
     {
         clrscr();
@@ -1627,6 +1628,7 @@ void redraw_console_sidebar()
         CPRINTF("Your terminal window is too small; please resize to at least %d,%d", MIN_COLS, MIN_LINES);
         return;
     }
+#endif
     // TODO: this is super hacky and merges stuff from redraw_screen and
     // viewwindow. It won't do nothing for webtiles, but should be basically
     // benign there.
@@ -1673,6 +1675,7 @@ void redraw_screen(bool show_updates)
         tiles.pop_all_ui_layouts();
 #endif
 
+#ifndef USE_TILE_LOCAL
     if (crawl_state.smallterm)
     {
         clrscr();
@@ -1680,6 +1683,7 @@ void redraw_screen(bool show_updates)
         CPRINTF("Your terminal window is too small; please resize to at least %d,%d", MIN_COLS, MIN_LINES);
         return;
     }
+#endif
 
     draw_border();
 
