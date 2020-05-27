473025ab83c759b17747895b186a4f3585af26fb
Umer Shaikh <umer.f.shaikh@gmail.com>
2020-05-26 23:25:20 -0500

    Simplify some pathfinding code
    Commit d63ab3c5 ("Simplify monster intelligence") made it impossible for
    mons_tracking_range to return a value of 0 (signifying no range
    restriction, for highly intelligent monsters). Therefore there is no
    need for conditionals checking to see if that value is greater than 0,
    and this commit removes some of the special handling for that
    case. (Probably a more thorough pass can find more of this.)

--- crawl-ref/source/mon-pathfind.cc
+++ crawl-ref/source/mon-pathfind.cc
@@ -45,21 +45,20 @@ int mons_tracking_range(const monster* mon)
         break;
     }
 
-    if (range)
-    {
-        if (mon->can_cling_to_walls())
-            range += 4;
-        else if (mons_is_native_in_branch(*mon))
-            range += 3;
-        else if (mons_class_flag(mon->type, M_BLOOD_SCENT))
-            range++;
-    }
+    if (mon->can_cling_to_walls())
+        range += 4;
+    else if (mons_is_native_in_branch(*mon))
+        range += 3;
+    else if (mons_class_flag(mon->type, M_BLOOD_SCENT))
+        range++;
 
     if (player_under_penance(GOD_ASHENZARI))
         range *= 5;
 
     if (mons_foe_is_marked(*mon) || mon->has_ench(ENCH_HAUNTING))
         range *= 5;
+
+    ASSERT(range);
 
     return range;
 }
