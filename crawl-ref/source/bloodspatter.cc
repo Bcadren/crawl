133ff0f31b7aaa8122948ebb25b44c8efe3811d0
Umer Shaikh <umer.f.shaikh@gmail.com>
2020-05-27 20:31:16 -0500

    Correct an outdated comment

--- crawl-ref/source/bloodspatter.cc
+++ crawl-ref/source/bloodspatter.cc
@@ -172,9 +172,9 @@ static void _maybe_bloodify_square(const coord_def& where, int amount,
     }
 }
 
-// Currently flavour only: colour ground (and possibly adjacent squares) red.
-// "damage" depends on damage taken (or hitpoints, if damage higher),
-// or, for sacrifices, on the number of chunks possible to get out of a corpse.
+// Colour ground (and possibly adjacent squares) red. "damage" depends on damage
+// taken (or hitpoints, if damage higher), or, for butchering, on the number of
+// chunks possible to get out of a corpse.
 void bleed_onto_floor(const coord_def& where, monster_type montype,
                       int damage, bool spatter, bool smell_alert,
                       const coord_def& from, const bool old_blood)
