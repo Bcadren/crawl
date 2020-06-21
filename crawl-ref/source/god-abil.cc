d737367e3c7564130ab34222ecd42d52a3dfff6a
Aidan Holm <aidanholm@gmail.com>
2020-06-21 20:01:04 +0800

    Fix inadvertent copies in loops

--- crawl-ref/source/god-abil.cc
+++ crawl-ref/source/god-abil.cc
@@ -5582,7 +5582,7 @@ static void _choose_arcana_mutations()
         = you.props[ARCANA_SAC_KEY].get_vector();
     ASSERT(current_arcane_sacrifices.empty());
 
-    for (const vector<mutation_type> arcane_sacrifice_list :
+    for (const vector<mutation_type> &arcane_sacrifice_list :
                                     _arcane_sacrifice_lists)
     {
         const mutation_type sacrifice =
@@ -5602,7 +5602,7 @@ static void _choose_arcana_mutations()
  */
 static bool _player_sacrificed_arcana()
 {
-    for (const vector<mutation_type> arcane_sacrifice_list :
+    for (const vector<mutation_type> &arcane_sacrifice_list :
                                     _arcane_sacrifice_lists)
     {
         for (mutation_type sacrifice : arcane_sacrifice_list)
