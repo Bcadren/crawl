4654a4248d21b74ec358fa378f7322c9238e36c6
Aidan Holm <aidanholm@gmail.com>
2020-06-27 14:33:23 +0800

    Render dungeon zaps onto vbuf
    This commit merges the rendering of zaps into the vbuf. Previously,
    these were rendered separately via add_overlay() and clear_overlays() on
    tiles, and blitted directly to the output buffer on console.
    
    That approach had a number of issues. First, resizing the local tiles
    window when zaps were shown caused them to vanish; this was easy to
    reproduce when resizing the window while unlocking the entrance to Zot.
    It involved global state that had to be kept in sync with vbuf updates.
    
    It was also a poor fit for the game semantics; zaps are caused by
    in-game elements that are part of the game simulation, so they should be
    rendered in the vbuf, unlike objects like cursors that are part of the
    UI.
    
    This commit has a number of parts. First, the calls to draw zaps were
    replaced with new interfaces that are implemented in view.cc. There is
    still some static state here, unfortunately, but this allows zaps to at
    least be rendered to the vbuf. Next, zaps are rendered into the vbuf, at
    the bottom of draw_cell().
    
    Actually drawing this vbuf requires minor modifications to rendering
    code: on local tiles, overlays (and all other tiles for each cell) are
    sorted by virtue of the fact that the local tiles renderer must pack
    each overlay tile into separate per-texture buffers. Webtiles has no
    such restriction, and so we must render overlays from different textures
    in two steps to get the desired effect (zaps on top of everything).

--- crawl-ref/source/tileweb.h
+++ crawl-ref/source/tileweb.h
@@ -133,9 +133,6 @@ public:
 
     const coord_def &get_cursor() const;
 
-    void add_overlay(const coord_def &gc, tileidx_t idx);
-    void clear_overlays();
-
     void draw_doll_edit();
 
     // Webtiles-specific
--- crawl-ref/source/view.cc
+++ crawl-ref/source/view.cc
@@ -1286,6 +1286,8 @@ static bool _viewwindow_should_render()
     return !run_dont_draw;
 }
 
+void _view_clear_overlays();
+
 /**
  * Draws the main window using the character set returned
  * by get_show_glyph().
@@ -1356,9 +1358,9 @@ void viewwindow(bool show_updates, bool tiles_only, animation *a)
 #ifdef USE_TILE
             tile_draw_floor();
             tile_draw_rays(true);
-            tiles.clear_overlays();
             tile_draw_map_cells();
 #endif
+            _view_clear_overlays();
         }
 
         if (show_updates)
@@ -1394,6 +1396,44 @@ void viewwindow(bool show_updates, bool tiles_only, animation *a)
         if (_layers != LAYERS_ALL)
             show_init();
     }
+}
+
+#ifdef USE_TILE
+struct tile_overlay
+{
+    coord_def gc;
+    tileidx_t tile;
+};
+static vector<tile_overlay> tile_overlays;
+
+void view_add_tile_overlay(const coord_def &gc, tileidx_t tile)
+{
+    tile_overlays.push_back({gc, tile});
+}
+#endif
+
+#ifndef USE_TILE_LOCAL
+struct glyph_overlay
+{
+    coord_def gc;
+    cglyph_t glyph;
+};
+static vector<glyph_overlay> glyph_overlays;
+
+void view_add_glyph_overlay(const coord_def &gc, cglyph_t glyph)
+{
+    glyph_overlays.push_back({gc, glyph});
+}
+#endif
+
+void _view_clear_overlays()
+{
+#ifdef USE_TILE
+    tile_overlays.clear();
+#endif
+#ifndef USE_TILE_LOCAL
+    glyph_overlays.clear();
+#endif
 }
 
 /**
@@ -1552,6 +1592,21 @@ void draw_cell(screen_cell_t *cell, const coord_def &gc,
         else if (is_excluded(gc))
             cell->colour = Options.tc_exclude_circle;
     }
+#endif
+
+#ifdef USE_TILE
+    for (const auto &overlay : tile_overlays)
+        if (overlay.gc == gc)
+            cell->tile.dngn_overlay[cell->tile.num_dngn_overlay++] = overlay.tile;
+#endif
+
+#ifndef USE_TILE_LOCAL
+    for (const auto &overlay : glyph_overlays)
+        if (overlay.gc == gc)
+        {
+            cell->glyph = overlay.glyph.ch;
+            cell->colour = overlay.glyph.col;
+        }
 #endif
 }
 
