74b2b8a5dfeef0dccf2c0947503f02e0f2a4770f
Aidan Holm <aidanholm@gmail.com>
2020-07-07 02:01:34 +0800

    Inject custom rendering into viewwindow()
    A longstanding source of rendering bugs in crawl is the accretion of
    various bits of global state used to draw various parts of the main
    UI view, all interacted with in various ways both from viewwindow
    itself and from the parts of crawl that alter rendering (e.g. rendering
    zaps, highlighting summoners, drawing beams, etc.). Good examples of
    this are anything that uses view_add_tile_overlay in conjunction with
    viewwindow().
    
    The result is that each part of crawl that does any rendering at all is
    extremely brittle, since it must depend on this complex global state,
    and has to know what the current state of that global state is. This
    makes each piece of rendering code work fine most of the time, but
    inevitably break when used from a different context which violates these
    assumptions. It also makes some things difficult to implement or very
    inefficient because multiple viewwindow calls are required.
    
    One of the worst offenders: directn. The direction chooser's drawing
    code draws beams and highlights summoners. It used to do this via a
    complicated series of calls to viewwindow() that was required in order
    to add tile overlays, then writing directly to the screen to overwrite
    the console output with the required alterations.
    
    The new code works by allowing the injection of custom render behaviour
    into the viewwindow() call. This allows any kind of custom rendering,
    instead of limited functions hard-coded directly into viewwindow(),
    while at the same time limiting the scope of these modifications; there
    is no global scope. You also don't need to understand the viewport
    rendering system; simply implement a type and provide an instance, and
    viewwindow() will call its callbacks when needed. You don't need to add
    tiles to a global buffer or blit directly to the screen; just modify the
    provided crawl_view_buffer.
    
    A nice side effect of this is the near eradication of the tiles_only
    parameter to viewwindow(). The only remaining use is in tileweb.cc,
    where it is required to prevent scribbling over the console map display
    while panning the tiles vbuf. One possible approach I am considering is
    to inject the rendering behaviour of the base map itself, but that's a
    change for another commit.

--- crawl-ref/source/view.cc
+++ crawl-ref/source/view.cc
@@ -1275,7 +1275,7 @@ void run_animation(animation_type anim, use_animation_type type, bool cleanup)
 
 static bool _view_is_updating = false;
 
-crawl_view_buffer view_dungeon(animation *a, bool anim_updates);
+crawl_view_buffer view_dungeon(animation *a, bool anim_updates, view_renderer *renderer);
 
 static bool _viewwindow_should_render()
 {
@@ -1300,8 +1300,9 @@ static bool _viewwindow_should_render()
  * @param tiles_only if true, only the tile view will be updated. This
  *                   is only relevant for Webtiles.
  * @param a[in] the animation to be showing, if any.
+ * @param renderer[in] A view renderer used to inject extra visual elements.
  */
-void viewwindow(bool show_updates, bool tiles_only, animation *a)
+void viewwindow(bool show_updates, bool tiles_only, animation *a, view_renderer *renderer)
 {
     if (_view_is_updating)
     {
@@ -1371,7 +1372,7 @@ void viewwindow(bool show_updates, bool tiles_only, animation *a)
 
         if (_viewwindow_should_render())
         {
-            const auto vbuf = view_dungeon(a, anim_updates);
+            const auto vbuf = view_dungeon(a, anim_updates, renderer);
 
             you.last_view_update = you.num_turns;
 #ifndef USE_TILE_LOCAL
@@ -1445,7 +1446,7 @@ void view_clear_overlays()
  * @param a[in] the animation to be showing, if any.
  * @return A new view buffer with the rendered content.
  */
-crawl_view_buffer view_dungeon(animation *a, bool anim_updates)
+crawl_view_buffer view_dungeon(animation *a, bool anim_updates, view_renderer *renderer)
 {
     crawl_view_buffer vbuf(crawl_view.viewsz);
 
@@ -1473,6 +1474,9 @@ crawl_view_buffer view_dungeon(animation *a, bool anim_updates)
 
         cell++;
     }
+
+    if (renderer)
+        renderer->render(vbuf);
 
     return vbuf;
 }
