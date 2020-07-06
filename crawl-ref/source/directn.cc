e7ebb072ccfe54ba7e494236109447e92119d760
Aidan Holm <aidanholm@gmail.com>
2020-07-07 02:01:57 +0800

    Remove needless use of viewwindow

--- crawl-ref/source/directn.cc
+++ crawl-ref/source/directn.cc
@@ -165,10 +165,6 @@ static void _wizard_make_friendly(monster* m)
         break;
     }
     mons_att_changed(m);
-
-    // To update visual branding of friendlies. Only seems capabable
-    // of adding bolding, not removing it, though.
-    viewwindow();
 }
 #endif
 
