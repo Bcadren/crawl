cc0311c69ff82da10b06449f72fa2bf22b4b9251
advil <rawlins@gmail.com>
2020-07-11 19:25:10 -0400

    Don't let Xom add to weight 0 innate mutations (Lightli)
    Other code paths check weight differently than this but Xom uses the
    weight calculations in _accept_mutation, and they provide a small chance
    of adding on to any innate mutations that are weight 0. One could
    imagine that this might have once been intended (because Xom) but it's
    not written in such a way that makes me think this is likely; I'm also
    pretty sure Xom can't remove such mutations.

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -1088,6 +1088,9 @@ static bool _accept_mutation(mutation_type mutat, bool ignore_weight, bool temp)
     if (ignore_weight)
         return true;
 
+    if (mdef.weight == 0)
+        return false;
+
     // bias towards adding (non-innate) levels to existing innate mutations.
     const int weight = mdef.weight + you.get_innate_mutation_level(mutat);
 
