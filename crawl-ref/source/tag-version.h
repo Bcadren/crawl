f74c4cb60f7b2a477dc0cb36d306bf17450d5794
Kate <601195+semitonal@users.noreply.github.com>
2020-07-12 04:38:07 +0100

    Remove unused trap code

--- crawl-ref/source/tag-version.h
+++ crawl-ref/source/tag-version.h
@@ -249,6 +249,7 @@ enum tag_minor_version
     TAG_MINOR_TRACK_REGEN_ITEMS,   // Regen items take effect only after maxhp is reached
     TAG_MINOR_MORGUE_SCREENSHOTS,  // Screenshots morgue section
     TAG_MINOR_MONSTER_TYPE_SIZE,   // Consistently marshall monster_type enums
+    TAG_MINOR_REVEALED_TRAPS,      // No skill check to spot traps
 #endif
     NUM_TAG_MINORS,
     TAG_MINOR_VERSION = NUM_TAG_MINORS - 1
