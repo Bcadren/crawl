7ecbefd902f11afa8df33b79ae1926762104dfdb
Nicholas Feinberg <pleasingfung@gmail.com>
2020-07-14 20:39:08 -0700

    Remove debug code

--- crawl-ref/source/spl-transloc.cc
+++ crawl-ref/source/spl-transloc.cc
@@ -426,12 +426,6 @@ spret frog_hop(bool fail, bool spider)
 }
 
 static bool _check_charge_through(coord_def pos) {
-/*    if (is_feat_dangerous(grd(pos)))
-    {
-        canned_msg(MSG_UNTHINKING_ACT);
-        return false;
-    }*/
-
     if (!you.can_pass_through_feat(grd(pos)))
     {
         clear_messages();
