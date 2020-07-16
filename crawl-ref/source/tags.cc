f59354708914536c89d681dbd6ecc05f8d6cc990
Edgar A. Bering IV <trizor@gmail.com>
2020-07-16 15:55:01 -0400

    Don't remove fixup spells from the library (Shummie)
    c881fc3179 introduced compatibility code to fixup removed spells
    regardless of tag-minor status; unfortunately this resulted in spells
    with a replacement being removed from newer saves.

--- crawl-ref/source/tags.cc
+++ crawl-ref/source/tags.cc
@@ -2380,7 +2380,10 @@ static void _fixup_library_spells(FixedBitVector<NUM_SPELLS>& lib, int version)
             lib.set(i, false);
         else if (newspell != (spell_type)i)
         {
-            lib.set(newspell, lib[i]);
+            // Only give the fixup if they had the spell, don't remove
+            // replacements
+            if (lib[i])
+                lib.set(newspell, lib[i]);
             lib.set(i, false);
         }
 
