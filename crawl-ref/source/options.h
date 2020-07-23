d222f0309315bdbbcd0fad4569d2b297bcaf3f08
Edgar A. Bering IV <trizor@gmail.com>
2020-07-23 00:28:41 -0400

    New regex list option monster_item_view_features
    Add an ability to customize the features listed in ^x. This is intended
    as an accessibility feature to allow visually impaired players to
    request a customizably more verbose ^x listing describing the dungeon
    around them.

--- crawl-ref/source/options.h
+++ crawl-ref/source/options.h
@@ -203,6 +203,7 @@ public:
     int         msg_webtiles_height;
     bool        mlist_allow_alternate_layout;
     bool        monster_item_view_coordinates;
+    vector<text_pattern> monster_item_view_features;
     bool        messages_at_top;
     bool        msg_condense_repeats;
     bool        msg_condense_short;
