c29aff2e91531b0d933c825c07499e2aae496122
wheals <whealswithinwheals@gmail.com>
2020-08-23 22:57:26 +0300

    Don't pop up a menu for self-targetting.
    It looks bad on webtiles and terrible on console.

--- crawl-ref/source/directn.cc
+++ crawl-ref/source/directn.cc
@@ -925,7 +925,8 @@ bool direction_chooser::move_is_ok() const
                          && Options.allow_self_target
                                 != confirm_prompt_type::none)
                 {
-                    return yesno("Really target yourself?", false, 'n');
+                    return yesno("Really target yourself?", false, 'n',
+                                 true, true, false, nullptr, false);
                 }
             }
 
