d7aee341b7e5c8e08850eca7cc204cfdae1f0b57
advil <rawlins@gmail.com>
2020-10-02 12:21:38 -0400

    Fix temp on several uselessness checks
    Also switch capitalization so it conforms to everything else.

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1541,10 +1541,10 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
     case SPELL_PASSWALL:
         // the full check would need a real spellpower here, so we just check
         // a drastically simplified version of it
-        if (temp && !passwall_simplified_check(you))
-            return "you aren't next to any passable walls.";
         if (temp && you.is_stationary())
             return "you can't move.";
+        if (temp && !passwall_simplified_check(you))
+            return "you aren't next to any passable walls.";
         break;
 
     case SPELL_SKELETAL_UPRISING:
@@ -1552,13 +1552,13 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
             return "skeletons already rise from your steps.";
         // intentional fallthrough
     case SPELL_ANIMATE_DEAD:
-        if (!animate_dead(&you, 1, BEH_FRIENDLY, MHITYOU, &you, "", GOD_NO_GOD, false))
-            return "There is nothing nearby to animate!";
+        if (temp && !animate_dead(&you, 1, BEH_FRIENDLY, MHITYOU, &you, "", GOD_NO_GOD, false))
+            return "there is nothing nearby to animate!";
         break;
 
     case SPELL_SIMULACRUM:
-        if (find_simulacrable_corpse(you.pos()) < 0)
-            return "There is nothing here to animate!";
+        if (temp && find_simulacrable_corpse(you.pos()) < 0)
+            return "there is nothing here to animate!";
         break;
 
     case SPELL_SONG_OF_SLAYING:
@@ -1567,8 +1567,8 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
         break;
 
     case SPELL_CORPSE_ROT:
-        if (corpse_rot(&you, false) == spret::abort)
-            return "There is nothing fresh enough to decay nearby.";
+        if (temp && corpse_rot(&you, false) == spret::abort)
+            return "there is nothing fresh enough to decay nearby.";
         // fallthrough
     case SPELL_POISONOUS_VAPOURS:
     case SPELL_CONJURE_FLAME:
