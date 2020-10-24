5a5e8210ad7562c0ec292dcb8cbfcbe75228f05f
Aidan Holm <aidanholm@gmail.com>
2020-10-24 23:47:25 +0800

    Make some mon-ench methods private

--- crawl-ref/source/monster.h
+++ crawl-ref/source/monster.h
@@ -209,10 +209,7 @@ public:
 
     void react_to_damage(const actor *oppressor, int damage, beam_type flavour);
 
-    void add_enchantment_effect(const mon_enchant &me, bool quiet = false);
-    void remove_enchantment_effect(const mon_enchant &me, bool quiet = false);
     void apply_enchantments();
-    void apply_enchantment(const mon_enchant &me);
 
     void degenerate(const actor *f);
 
@@ -643,4 +640,8 @@ private:
                               int &nvalid) const;
     bool search_spells(function<bool (spell_type)> func) const;
     bool is_cloud_safe(const coord_def &place) const;
+
+    void add_enchantment_effect(const mon_enchant &me, bool quiet = false);
+    void remove_enchantment_effect(const mon_enchant &me, bool quiet = false);
+    void apply_enchantment(const mon_enchant &me);
 };
