f011b7fa606f4a88cad725dcb3d54f020dae073b
Nicholas Feinberg <pleasingfung@gmail.com>
2020-10-25 13:23:37 -0700

    New monster: fenstrider witch
    Like Baba Yaga, but instead of putting chicken legs on her hut, she
    just wears em straight up. Wanders around tossing paralyse and
    melee agony and Toxic Bog attacks at the player. Intended as a
    top-tier Swamp threat.

--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -408,6 +408,7 @@ enum monster_type                      // env.mons[].type
     MONS_TENGU_WARRIOR,
     MONS_TENGU_CONJURER,
     MONS_TENGU_REAVER,
+    MONS_FENSTRIDER_WITCH,
 #endif
     MONS_MINOTAUR,
     MONS_NAGA,
@@ -1063,6 +1064,7 @@ enum monster_type                      // env.mons[].type
     MONS_DEVOURING_SWARM,
     MONS_GOLIATH_FROG,
     MONS_ELEIONOMA,
+    MONS_FENSTRIDER_WITCH,
 #endif
 
     NUM_MONSTERS,               // used for polymorph
