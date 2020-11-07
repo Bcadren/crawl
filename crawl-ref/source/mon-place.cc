d9573807191a7cf5bdc59a373f73573d407088fa
Nicholas Feinberg <pleasingfung@gmail.com>
2020-11-07 07:41:35 -0800

    Fix polymorphing band members (advil)
    Ringo, look out!

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -810,8 +810,6 @@ monster* place_monster(mgen_data mg, bool force_pos, bool dont_place)
     if (!mon)
         return nullptr;
 
-    init_poly_set(mon);
-
     if (mg.props.exists(MAP_KEY))
         mon->set_originating_map(mg.props[MAP_KEY].get_string());
 
@@ -1631,6 +1629,8 @@ static monster* _place_monster_aux(const mgen_data &mg, const monster *leader,
     }
 
     tile_init_props(mon);
+
+    init_poly_set(mon);
 
 #ifndef DEBUG_DIAGNOSTICS
     // A rare case of a debug message NOT showing in the debug mode.
