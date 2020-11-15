fbfeabf0e9ccffdb387f6fa11a77d62cdc3ca1ab
advil <rawlins@gmail.com>
2020-11-15 14:18:51 -0500

    Implement target preselection for artefacts
    There is only one artefact that currently needs this, the staff of
    dispater. This finishes out the API for allowing target preselection for
    everything that can be triggered via `evoke_item`.

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -2223,7 +2223,8 @@ bool item_is_evokable(const item_def &item, bool reach,
     {
         const unrandart_entry* entry = get_unrand_entry(item.unrand_idx);
 
-        if (entry->evoke_func && item_type_known(item))
+        if ((entry->evoke_func || entry->targeted_evoke_func)
+            && item_type_known(item))
         {
             if (no_evocables)
             {
