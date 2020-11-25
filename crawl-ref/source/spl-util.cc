f3995e85abcdb354bd49fd3c0d32095a09f7496e
advil <rawlins@gmail.com>
2020-11-25 13:59:00 -0500

    More spell quivering fixes, this time for excursions
    81e999faa9 was specifically triggered by checking spell uselessness of a
    quivered spell while doing levelgen. Crashes can also happen if this is
    checked during an excursion. Before, none of this code would have a way
    of being called, but when a spell is quivered on newquivers, this code
    is used to figure out whether to gray out the spell, so it is triggered
    by ui redraws as well as sending player info in webtiles. I've added
    another sanity check to `spell_no_hostile_in_range`, but also just
    headed this off at the outset in `spell_uselessness_reason`.

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1391,6 +1391,10 @@ bool spell_is_kiku_ritual(spell_type spell)
 string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
                                 bool skip_casting_checks)
 {
+    // prevent all of this logic during excursions / levelgen
+    if (!in_bounds(you.pos()) || !you.on_current_level)
+        return "you can't cast spells right now.";
+
     if (!skip_casting_checks)
     {
         string c_check = casting_uselessness_reason(spell, temp);
@@ -1689,8 +1693,10 @@ int spell_highlight_by_utility(spell_type spell, int default_colour,
 
 bool spell_no_hostile_in_range(spell_type spell)
 {
-    if (!in_bounds(you.pos()))
+    // sanity check: various things below will be prone to crash in these cases.
+    if (!in_bounds(you.pos()) || !you.on_current_level)
         return true;
+
     const int range = calc_spell_range(spell, 0);
     const int minRange = get_dist_to_nearest_monster();
 
