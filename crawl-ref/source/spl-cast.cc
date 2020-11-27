3bf5ddfb2e305bbce95cd5ba61745276047b892d
advil <rawlins@gmail.com>
2020-11-27 13:06:45 -0500

    Don't show static targeters for `Z`
    People seemed confused by this, so I'm removing them for now. I can't
    tell whether this confusion is because of change or intrinsic confusion.
    It's true that many static targeters are extremely boring (e.g.
    self-targeted ones and summon ones that just show range 2). I'm leaving
    them for the fire interface, because even though I've also seen this
    described as "weird" it's pretty important for consistency, and if you
    want to skip the targeter you can just use `p` or shift-tab in this
    mode. (Also, I do really think these targeters are extremely useful for
    new players.)
    
    What might be best is if z/Z both had a toggle that let you choose to
    see the static targeter if you want to check range.

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -1211,7 +1211,7 @@ bool cast_a_spell(bool check_range, spell_type spell, dist *_target)
     const int cost = spell_mana(spell);
     dec_mp(cost, true);
 
-    const spret cast_result = your_spells(spell, 0, true, nullptr, _target, check_range);
+    const spret cast_result = your_spells(spell, 0, true, nullptr, _target);
     if (cast_result == spret::abort)
     {
         crawl_state.zero_turns_taken();
@@ -1883,8 +1883,7 @@ private:
  * the casting.
  **/
 spret your_spells(spell_type spell, int powc, bool allow_fail,
-                       const item_def* const evoked_item,
-                       dist *target, bool range_checked)
+                       const item_def* const evoked_item, dist *target)
 {
     ASSERT(!crawl_state.game_is_arena());
     ASSERT(!evoked_item || evoked_item->base_type == OBJ_WANDS);
@@ -1934,7 +1933,8 @@ spret your_spells(spell_type spell, int powc, bool allow_fail,
     // because of it). Hopefully, those will eventually be fixed. - bwr
     // TODO: what's the status of the above comment in 2020+?
     if (is_targeted
-        || hitfunc && (!range_checked || Options.always_use_static_targeters))
+        // force static targeters when called in "fire" mode
+        || hitfunc && (target->fire_context || Options.always_use_static_targeters))
     {
         const targ_mode_type targ =
               testbits(flags, spflag::neutral)    ? TARG_ANY :
