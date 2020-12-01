3b6ef2229b031cb1021310dd8f5dd4f00e08eade
advil <rawlins@gmail.com>
2020-12-01 16:13:55 -0500

    Prevent unnecessary redraws during autotravel (Goratrix)
    This gets quivers and a few other things that shouldn't be updated in
    fast autotravel modes. (In testing this I noticed that console, but not
    tiles, still prints messages with these modes, should maybe be fixed
    too.)

--- crawl-ref/source/output.cc
+++ crawl-ref/source/output.cc
@@ -607,9 +607,15 @@ static void _print_stats_equip(int x, int y)
  */
 static void _print_stats_noise(int x, int y)
 {
+    CGOTOXY(x, y, GOTO_STAT);
+    if (mouse_control::current_mode() == MOUSE_MODE_NORMAL
+        && (you.running > 0 || you.running < 0 && Options.travel_delay == -1))
+    {
+        return;
+    }
+
     bool silence = silenced(you.pos());
     int level = silence ? 0 : you.get_noise_perception(true);
-    CGOTOXY(x, y, GOTO_STAT);
     textcolour(HUD_CAPTION_COLOUR);
     cprintf("Noise: ");
     colour_t noisecolour;
@@ -1047,6 +1053,15 @@ static int _wpn_name_colour(int hand)
 static void _print_stats_wp(int hand, int y)
 {
     string text;
+
+    if (mouse_control::current_mode() == MOUSE_MODE_NORMAL
+        && (you.running > 0 || you.running < 0 && Options.travel_delay == -1))
+    {
+        return;
+    }
+
+    CGOTOXY(1, y, GOTO_STAT);
+
     if (hand == 1 && you.get_mutation_level(MUT_MISSING_HAND))
         text = "(unavailable)";
     else if (you.weapon(hand))
@@ -1063,7 +1078,6 @@ static void _print_stats_wp(int hand, int y)
     else
         text = you.unarmed_attack_name();
 
-    CGOTOXY(1, y, GOTO_STAT);
     textcolour(HUD_CAPTION_COLOUR);
     const char slot_letter = you.weapon(hand) ? index_to_letter(you.weapon(hand)->link)
                                           : '-';
@@ -1104,6 +1118,12 @@ static void _print_stats_wp(int hand, int y)
 static void _print_stats_qv(int y)
 {
     CGOTOXY(1, y, GOTO_STAT);
+    if (mouse_control::current_mode() == MOUSE_MODE_NORMAL
+        && (you.running > 0 || you.running < 0 && Options.travel_delay == -1))
+    {
+        return;
+    }
+
     formatted_string qdesc = quiver::get_secondary_action()->quiver_description();
 #ifdef USE_TILE_LOCAL
     const int max_width = crawl_view.hudsz.x - (tiles.is_using_small_layout() ? 0 : 4);
@@ -1147,6 +1167,8 @@ static void _add_status_light_to_out(int i, vector<status_light>& out)
 static void _get_status_lights(vector<status_light>& out)
 {
 #ifdef DEBUG_DIAGNOSTICS
+    if (mouse_control::current_mode() != MOUSE_MODE_NORMAL
+        || !(you.running > 0 || you.running < 0 && Options.travel_delay == -1))
     {
         static char static_pos_buf[80];
         snprintf(static_pos_buf, sizeof(static_pos_buf),
