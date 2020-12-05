5054d8d8492d017776f507ddc0f68a1e08986093
Nicholas Feinberg <pleasingfung@gmail.com>
2020-12-05 08:30:42 -0800

    Show player spell accuracy
    As + pips, since a raw accuracy number means very little. Ranges
    from about 2 pips for some 0-power spells to 7 pips for max-power LCS.
    Should be a nice complement to the damage numbers.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -3860,6 +3860,9 @@ static string _player_spell_stats(const spell_type spell)
         description += "\nDamage : ";
         description += damage_string;
     }
+    const int acc = spell_acc(spell);
+    if (acc != -1)
+        description += make_stringf("\nAccuracy: %d%%", acc);
 
     description += "\nRange : ";
     description += spell_range_string(spell);
--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -2961,6 +2961,22 @@ string spell_damage_string(spell_type spell)
             dam.num, dam.size, suffix.c_str());
 }
 
+int spell_acc(spell_type spell)
+{
+    const zap_type zap = spell_to_zap(spell);
+    if (zap == NUM_ZAPS)
+        return -1;
+    if (zap_explodes(zap) || zap_is_enchantment(zap))
+        return -1;
+    const int power = _spell_power(spell);
+    if (power < 0)
+        return -1;
+    const int acc = zap_to_hit(zap, power, false);
+    if (acc == AUTOMATIC_HIT)
+        return -1;
+    return acc;
+}
+
 string spell_power_string(spell_type spell)
 {
     const int cap = spell_power_cap(spell);
