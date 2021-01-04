9a7c4e0e490941418ef5d34f2cd93bb12d1c7c36
advil <rawlins@gmail.com>
2021-01-04 10:21:34 -0500

    Simplify quiver verbs for misc evokables
    I tried a few times to make these flavorful + non-confusing, but never
    really got the second part down. Just use "Evoke".

--- crawl-ref/source/quiver.cc
+++ crawl-ref/source/quiver.cc
@@ -1546,16 +1546,7 @@ namespace quiver
 
         string quiver_verb() const override
         {
-            ASSERT(is_valid());
-            switch (you.inv[wand_slot].sub_type)
-            {
-            case MISC_HORN_OF_GERYON:
-                return "Blow";
-            case MISC_BOX_OF_BEASTS:
-                return "Open";
-            default:
-                return "Evoke";
-            }
+            return "Evoke";
         }
 
         bool is_targeted() const override
