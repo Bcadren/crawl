a20a63fb0d5602a94e9a5d911fa82863332e6f02
Lucien <bhylton7@gmail.com>
2022-08-08 17:00:48 -0400

    Remove superfluous checks from the AC change display code.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -2419,12 +2419,8 @@ static string _describe_armour(const item_def &item, bool verbose)
 
     }
 
-    if (can_wear_armour(item, false, true)
-        && item_ident(item, ISFLAG_KNOW_PLUSES)
-        && !is_shield(item))
-    {
+    if (can_wear_armour(item, false, true))
         description += _armour_ac_change(item);
-    }
 
     return description;
 }
