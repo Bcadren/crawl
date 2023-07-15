66bb424853cd42c30dec849faaa983fc2e240fcf
Lucien <bhylton7@gmail.com>
2023-07-15 05:24:57 -0400

    Fix tab.

--- crawl-ref/source/dat/clua/autofight.lua
+++ crawl-ref/source/dat/clua/autofight.lua
@@ -337,13 +337,7 @@ local function get_target(no_move)
 end
 
 local function attack_fire(x,y)
-  if AUTOFIGHT_FORCE_FIRE or not have_ranged() then
-    -- fire from quiver
-    crawl.do_targeted_command("CMD_FIRE", x, y, AUTOFIGHT_FIRE_STOP)
-  else
-    -- fire a wielded launcher
-    crawl.do_targeted_command("CMD_EVOKE_WIELDED", x, y, AUTOFIGHT_FIRE_STOP)
-  end
+  crawl.do_targeted_command("CMD_FIRE", x, y, AUTOFIGHT_FIRE_STOP)
 end
 
 local function attack_reach(x,y)
