5145d8cf814b23a2b29c471165ad3c559cdd9f99
Lucien <bhylton7@gmail.com>
2023-07-29 20:04:46 -0400

    Fix battlesphere with monster-only spells.

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -3100,7 +3100,7 @@ bool aim_battlesphere(actor* agent, spell_type spell, int powc, bolt& beam)
 
         // This is so that reflection and pathing rules for the parent beam
         // will be obeyed when figuring out what is being aimed at
-        zappy(ztype, powc, false, testbeam);
+        zappy(ztype, powc, agent->is_monster(), testbeam);
 
         battlesphere->props["firing_target"] = beam.target;
         battlesphere->props.erase("foe");
